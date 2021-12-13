3a5a0220f3e78e694b122a62ba3d2799d9759ebc
NightFrost <49632085+NightFrost42@users.noreply.github.com>
2021-12-13 14:48:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ class LegymPost:
         url = 'https://cpes.legym.cn/education/app/activity/getActivityList'
         data = {"name":"","campus":"","page":1,"size":10,"state":"","topicId":"","week":""}
         r = requests.post(url=url, headers=self.headers, data=json.dumps(data),verify=False)
-        print(json.dumps(r.json(), ensure_ascii=False).encode("UTF-8"))
+        print(base64.b64encode(json.dumps(r.json(), ensure_ascii=False)))
         activityList:list = r.json()['data']['items']
         lst = [item[key] for item in activityList for key in item]
         return lst
